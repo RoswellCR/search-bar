0bd55a469ee8edc403adf720d9b27ae47daa742f
RoswellCR <rosbelcr@gmail.com>
2022-08-22 17:08:31 -0400

    adding files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+// components
+import Profile from './components/Profile';
+import UserList from './components/UserList';
+
+//CONTEXT
+import UserState from './context-user/User/UserState';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+function App() {
+  return (
+    
+        <div className='container p-4'>
+          <h1>Hola Mundo<h/>
+        </div>
+    
+  );
+}
+
+export default App;
