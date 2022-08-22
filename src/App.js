c108de6d8e5719c03b9f85d04a67080d4dc0c99c
RoswellCR <rosbelcr@gmail.com>
2022-08-22 18:10:02 -0400

    categories

--- src/App.js
+++ src/App.js
@@ -1,18 +1,83 @@
+import React,{useState} from 'react';
 
-// components
-import Profile from './components/Profile';
-import UserList from './components/UserList';
-
-//CONTEXT
-import UserState from './context-user/User/UserState';
-
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+const people = [
+{
+  id: "people-01",
+  title: "Eli Losa"
+},
+{
+  id: "people-02",
+  title: "Ross Caballero"
+},
+{
+  id: "people-03",
+  title: "Pepe Perez"
+},
+{
+  id: "people-04",
+  title: "Paco Avila"
+},
+{
+  id: "people-05",
+  title: "Juan Diego"
+},
+]
+const calendar = [
+  {
+    id:"calendar-01",
+    title: "Ejercicios"
+  },
+  {
+    id:"calendar-02",
+    title: "Dormir"
+  },
+  {
+    id:"calendar-03",
+    title: "Comer"
+  },
+  {
+    id:"calendar-04",
+    title: "Programar"
+  },
+  {
+    id:"calendar-05",
+    title: "Leer"
+  },
+]
 
 function App() {
+  const [data, setData] = useState([...people, ...calendar])
+  const [selection, setSelection] = useState(null);
+  const [currentOpt, setCurrentOpt] = useState('all');
+
+  const handleClick = (e)=>{
+    const op = e.target.name;
+
+    switch (op) {
+      case 'all':
+        setData([...people, ...calendar])
+        setCurrentOpt('all')
+        break;
+      case 'people':
+        setData([...people])
+        setCurrentOpt('people')
+        break;
+      case 'calendar':
+       setData([...calendar])
+       setCurrentOpt('calendar')
+       break;       
+    
+      default:
+        break;
+    }
+  }
   return (
     
-        <div className='container p-4'>
-          <h1>Hola Mundo<h/>
+        <div>
+          <button onClick= {handleClick} name='all'> All </button>
+          <button onClick= {handleClick} name='people'> People </button>
+          <button onClick= {handleClick} name='calendar'> Calendar </button>
+          <div>{data.map(item=>item.title+',  ')}</div>
         </div>
     
   );
